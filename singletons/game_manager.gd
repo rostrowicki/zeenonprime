4572fe092ef034a04018b3fb30ffbec536908094
robert.ostrowicki <robert.ostrowicki@gmail.com>
2024-03-03 15:32:26 +0100

    update

---
+++ singletons/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var level_scene: PackedScene = preload("res://level/level.tscn")
+var main_scene: PackedScene = preload("res://main/main.tscn")
+
+func load_main_scene() -> void:
+	get_tree().change_scene_to_packed(main_scene)
+	
+func load_level_scene() -> void:
+	get_tree().change_scene_to_packed(level_scene)
