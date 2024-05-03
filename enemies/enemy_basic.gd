67b880df4021a5488229293d5cec364f6d72e084
robert.ostrowicki <robert.ostrowicki@gmail.com>
2024-05-03 11:18:32 +0200

    update

--- enemies/enemy_basic.gd
+++ enemies/enemy_basic.gd
@@ -3,6 +3,12 @@ extends PathFollow2D
 
 @export var shoots: bool = false
 @export var aims_at_player: bool = false
+@export var bullet_scene: PackedScene
+@export var bullet_damage: int = 10
+@export var bullet_speed: float = 200.0
+@export var bullet_direction: Vector2 = Vector2.DOWN
+@export var bullet_wait_time: float = 3.0 # seconds
+@export var bullet_wait_time_var: float = 0.05
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var laser_timer = $LaserTimer
@@ -33,12 +39,42 @@ func setup(speed: float, anim_name: String) -> void:
 	_speed = speed
 	_anim_string = anim_name
 
+
+func update_bullet_direction() -> void:
+	if (aims_at_player == false || is_instance_valid(_player_ref) == false ):
+		print("invalid", aims_at_player, is_instance_valid(_player_ref))
+		return
+	bullet_direction = global_position.direction_to(
+		_player_ref.global_position
+	)
+	
+
+func start_shoot_timer() -> void:
+	laser_timer.wait_time = randf_range(
+		bullet_wait_time - bullet_wait_time_var, 
+		bullet_wait_time + bullet_wait_time_var)
+	laser_timer.start()
+
+
+func shoot() -> void:
+	var b = bullet_scene.instantiate()
+	update_bullet_direction()
+	b.setup(
+		global_position,
+		bullet_direction,
+		bullet_speed,
+		bullet_damage
+	)
+	get_tree().root.add_child(b)
+	start_shoot_timer()
+
 func _on_laser_timer_timeout():
-	pass # Replace with function body.
+	shoot()
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered():
-	pass # Replace with function body.
+	if shoots == true:
+		start_shoot_timer()
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
